dbf62fed5d7a904df702c1de6f7c9e5aafb157b5
Noah <57935711+noahdaniel163@users.noreply.github.com>
2023-11-19 12:24:24 +0700

    WakeOnLan
    WakeOnLan

---
+++ WakeOnLan.ps1
@@ -0,0 +1,12 @@
+﻿$macAddress = "94-57-A5-ED-1F-8D"
+$macBytes = $macAddress -split '-' | ForEach-Object { [byte]('0x' + $_) }
+$magicPacket = @(0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF)
+
+for ($i = 0; $i -lt 16; $i++) {
+    $magicPacket += $macBytes
+}
+
+$udpClient = New-Object System.Net.Sockets.UdpClient
+$udpClient.Connect(([System.Net.IPAddress]::Broadcast), 9)
+$udpClient.Send($magicPacket, $magicPacket.Length)
+$udpClient.Close()
